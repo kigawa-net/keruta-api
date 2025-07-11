574a16ca768e6ce8ab9662746ffde441213cff80
kigawa <kigawa.8390@gmail.com>
2025-07-11 12:26:22 +0900

    wp

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,17 @@ repositories {
     mavenCentral()
 }
 
+// Suppress the warning about missing plugin descriptors
+// since we're only using buildSrc for dependency management
+gradlePlugin {
+    plugins {
+        register("empty") {
+            id = "empty"
+            implementationClass = "EmptyPlugin"
+        }
+    }
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
