69be70bae6d4da7377dba2755573e699733bc783
kigawa <mail@kigawa.net>
2025-09-25 17:21:24 +0000

    ｗ

--- src/main/kotlin/net/kigawa/keruta/api/session/controller/SessionController.kt
+++ src/main/kotlin/net/kigawa/keruta/api/session/controller/SessionController.kt
@@ -26,6 +26,7 @@ import java.time.ZoneOffset
 import net.kigawa.keruta.core.domain.model.Session as DomainSession
 
 @RestController
+@RequestMapping("/api/v1/sessions")
 class SessionController(
     private val sessionService: SessionService,
     private val sessionServiceImpl: SessionServiceImpl,
